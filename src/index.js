6e3f988510e7766580d1fc0b4531f2474e21b5f2
heysivani <heysivani@gmail.com>
2020-11-03 16:01:57 +0900

    if user is logged in, they can log out and be redirected to home page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
+import { auth, provider } from "./firebase";
 import "./index.css";
 import Home from "./components/Home/Home";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 
-function AppRouter() {
+function App() {
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        // check if a user is logged in
+        auth().onAuthStateChanged(user => {
+            if(user) {
+                // if a user is currently logged in, store them in state
+                setUser(user);
+            }
+        })
+    }, [])
+
+    const logoutUser = async () => {
+        await auth().signOut();
+        window.location = "/";
+    }
+
     return(
         <Router>
             <div className="app">
@@ -14,6 +32,7 @@ function AppRouter() {
                     <Link to="/">Home</Link>
                     <Link to="/login">Login</Link>
                     <Link to="/register">Register</Link>
+                    <a href = "#!" onClick={ logoutUser }>Logout</a>
                 </nav>
                 <Switch>
                     <Route path="/" exact component= { Home } />
@@ -29,4 +48,4 @@ function AppRouter() {
 const NoMatch = () => 
     "No space baes here :(";
 
-ReactDOM.render(<AppRouter />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
