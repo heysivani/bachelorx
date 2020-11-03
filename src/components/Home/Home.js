f1812af9e2c63d505f8436384cba6cc0c8b23b3b
heysivani <heysivani@gmail.com>
2020-11-03 16:37:25 +0900

    conditionally renders chatroom on homepage depending on login state

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,26 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Home.css";
 
-export default function Home() {
-    
+export default function Home({ user }) {
     return (
         <div className="home-container">
-            <h1>Home Container</h1>
+            <h1>Chatroom</h1>
+            { user &&
+                <div className="allow-chatroom">
+                    Chatroom here
+                </div>
+            }
+            { !user &&
+                <div className="deny-chatroom">
+                    <p>
+                        <Link to="/login">Login</Link>
+                        &nbsp;or&nbsp; 
+                        <Link to="/register">Register</Link>
+                        &nbsp;to enter the chatroom 😘
+                    </p>
+                </div>
+            }
         </div>
     );
 }
