0fbde79091010012a3347dfccb95342fe1d73108
heysivani <heysivani@gmail.com>
2020-11-03 12:26:18 +0900

    implemented route switching for home, login, and register components

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom";
 import "./index.css";
 import Home from "./components/Home/Home";
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 
-ReactDOM.render(<Home />, document.getElementById("root"));
+function AppRouter() {
+    return(
+        <Router>
+            <div className="app">
+                <nav className="main-nav">
+                    <Link to="/">Home</Link>
+                    <Link to="/login">Login</Link>
+                    <Link to="/register">Register</Link>
+                </nav>
+                <Switch>
+                    <Route path="/" exact component= { Home } />
+                    <Route path="/login" exact component= { Login } />
+                    <Route path="/register" exact component= { Register } />
+                    <Route component= { NoMatch } />
+                </Switch>
+            </div>
+        </Router>
+    );
+}
+
+const NoMatch = ({ location }) => 
+    "No route match for {location.pathname}";
+
+ReactDOM.render(<AppRouter />, document.getElementById("root"));
